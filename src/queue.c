227b6baecf52fdf77b1599da544e30e3976c7a83
ngyngcphu <nguyenngocphu225@gmail.com>
2023-05-10 22:57:06 +0700

    implementing queue.c

--- src/queue.c
+++ src/queue.c
@@ -2,19 +2,53 @@
 #include <stdlib.h>
 #include "queue.h"
 
-int empty(struct queue_t * q) {
-        if (q == NULL) return 1;
-	return (q->size == 0);
+int empty(struct queue_t *q)
+{
+        if (q == NULL)
+                return 1;
+        return (q->size == 0);
 }
 
-void enqueue(struct queue_t * q, struct pcb_t * proc) {
+void enqueue(struct queue_t *q, struct pcb_t *proc)
+{
         /* TODO: put a new process to queue [q] */
+        if (q == NULL)
+        {
+                perror("Queue is NULL !\n");
+                exit(1);
+        }
+        if (q->size == MAX_QUEUE_SIZE)
+        {
+                perror("Queue is full !\n");
+                exit(1);
+        }
+        q->proc[q->size] = proc;
+        q->size++;
 }
 
-struct pcb_t * dequeue(struct queue_t * q) {
+struct pcb_t *dequeue(struct queue_t *q)
+{
         /* TODO: return a pcb whose prioprity is the highest
          * in the queue [q] and remember to remove it from q
          * */
-	return NULL;
-}
+        if (q == NULL || q->size == 0)
+        {
+                return NULL;
+        }
+        struct pcb_t *temp = q->proc[0];
+        int index = 0;
+        int length = q->size;
+        for (int i = 1; i < length; ++i)
+        {
+                if (temp->priority < q->proc[i]->priority)
+                {
+                        temp = q->proc[i];
+                        index = i;
+                }
+        }
+        q->proc[index] = q->proc[length - 1];
+        q->proc[length - 1] = NULL;
+        q->size--;
 
+        return temp;
+}
